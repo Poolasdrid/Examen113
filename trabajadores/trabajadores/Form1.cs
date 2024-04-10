6fc29c0288135d3c30e97223b6fd7ce87805945f
Poolasdrid <poolasdrid@gmail.com>
2024-04-10 12:05:59 -0500

    agrego boton y check box

--- trabajadores/trabajadores/Form1.cs
+++ trabajadores/trabajadores/Form1.cs
@@ -8,5 +8,27 @@ namespace trabajadores
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'eMPRESADataSet.trabajadores' Puede moverla o quitarla según sea necesario.
+            this.trabajadoresTableAdapter.Fill(this.eMPRESADataSet.trabajadores);
+
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+
+        }
     }
 }
